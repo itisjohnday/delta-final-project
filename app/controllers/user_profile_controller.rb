3dcd1f86578e16d7a34bdcc744e1f9ba985f5442
gforceroy <gforceroy@hotmail.com>
2017-12-11 12:40:39 -0600

    final check to clean up controller

--- app/controllers/user_profile_controller.rb
+++ app/controllers/user_profile_controller.rb
@@ -1,25 +1,15 @@
 class UserProfileController < ApplicationController
-  before_action :find_user, only: [:show]
-  before_action :find_pets, only: [:index]
 
   def index
     @user = User.find(current_user.id)
   end
 
-  def find_user
-    @user = User.find(params[:id])
-  end
-
   def user_pets
-    @pets = Pet.where(user_id: 1)
+    @pets = Pet.where(user_id: current_user.id)
   end
 
   def user_info
-    @user = User.find(1)
-  end
-
-  def find_pets
-    @pets = Pet.where(username: 'philton')
+    @user = User.find(current_user.id)
   end
 
 end
