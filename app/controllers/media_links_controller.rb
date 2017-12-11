3739495a9caa5f04957e227696a3fc879b8022e8
ansachs <ansachs@gmail.com>
2017-12-11 11:06:50 -0600

    pre implmenting amazon bucket

--- app/controllers/media_links_controller.rb
+++ app/controllers/media_links_controller.rb
@@ -1,53 +1,52 @@
 class MediaLinksController < ApplicationController
   # before_action :authenticate_user!, except: [:index]
-  # before_action :set_pet, except: [:index, :new, :create]
+  before_action :set_user
 
+  def index
+    @links = MediaLink.where(user_id: @user.id)
+  end
 
-  # def index
-  #   @media_link = MediaLink.all
-  # end
+  def show
 
-  # def show
+  end
 
-  # end
+  def new
+    @pet = MediaLink.new
+  end
 
-  # def new
-  #   @pet = MediaLink.new
-  # end
-
-  # def create
-  #   @pet = MediaLink.new(pet_params)
-  #   @pet.user_id = current_user.id
+  def create
+    @pet = MediaLink.new(pet_params)
+    @pet.user_id = current_user.id
     
-  #   if @pet.save
-  #     redirect_to pets_path, notice: 'pet was successfully created.'
-  #   else
-  #     render :new
-  #   end
-  # end
+    if @pet.save
+      redirect_to pets_path, notice: 'pet was successfully created.'
+    else
+      render :new
+    end
+  end
 
-  # def edit
-  # end
+  def edit
+  end
 
-  # def update
-  #   if @pet.update(pet_params)
-  #     redirect_to pets_path, notice: 'pet was successfully updated.'
-  #   else
-  #     render :edit
-  #   end
-  # end
+  def update
+    if @pet.update(pet_params)
+      redirect_to pets_path, notice: 'pet was successfully updated.'
+    else
+      render :edit
+    end
+  end
 
-  # def destroy
-  #   @pet.destroy
-  #   redirect_to pets_path, notice: 'pet was successfully removed.'
-  # end
+  def destroy
+    @pet.destroy
+    redirect_to pets_path, notice: 'pet was successfully removed.'
+  end
 
-  # private
-  # def pet_params  
-  #   params['pet'].permit(:name, :breed)
-  # end
+  private
+  def pet_params  
+    params['pet'].permit(:name, :breed)
+  end
 
-  # def set_pet
-  #   @pet = Pet.find(params[:id])
-  # end
+  def set_user
+    @user = User.find(params[:user_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,4 @@ Rails.application.routes.draw do
   resources :users do
     resources :media_links
   end
-
 end
