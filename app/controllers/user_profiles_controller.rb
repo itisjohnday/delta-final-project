3276c36cea5f92a0f2cdadfe06b8614caa367365
ansachs <ansachs@gmail.com>
2017-12-13 00:28:04 -0600

    upload pictures functions, will associate with pets

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -7,6 +7,9 @@ class UserProfilesController < ApplicationController
 
   def show
     @pets = Pet.where(user_id: @user.id)
+    if current_user && params[:id] == current_user.id.to_s
+      render 'show_logged_in'
+    end
   end
 
   private
